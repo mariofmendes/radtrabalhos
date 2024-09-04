74efa4396176c9610a22bcb6593b91168883fa95
mariofmendes <50730642+mariofmendes@users.noreply.github.com>
2024-09-04 14:42:35 -0300

    Exercícios Aula 01 - Estrutura de Sequencial

---
+++ Aula 01 - Atividade Prática - Estrutura de Sequencial - Ex010.py
@@ -0,0 +1,7 @@
+"""10 - Faça um programa que peça a temperatura em graus Celsius e a transforme em graus Fahrenheit."""
+
+celsius = float(input("Informe a temperatura em graus Celsius: "))
+
+far = celsius * 1.8 + 32
+
+print("A temperatura em graus Celsius convertida para graus Fahrenheit é: ", far, "ºF")
