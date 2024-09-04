577bc217e90f81a3887a149e1cb36910832bc354
mariofmendes <50730642+mariofmendes@users.noreply.github.com>
2024-09-04 14:51:43 -0300

    Aula 01 - Atividade Verificadora de Aprendizagem

---
+++ Aula 01 - Atividade Verificadora de Aprendizagem - Questão 1.py
@@ -0,0 +1,14 @@
+"""Atividade Autônoma Aura:
+Questão 1) IADES 2018 ARCON (PA) Assistente Técnico em Regulação de Serviços Públicos Provas:
+Uma das técnicas de licitação de requisitos tem foco na promoção da cooperação, de
+entendimento e de trabalho em grupo entre os desenvolvedores, gerando uma visão compartilhada
+do que o produto deve ser. Essa técnica é conhecida como :
+a) levantamento orientado a pontos de vista.
+b) brainstorming.
+c) JAD.
+d) entrevistas.
+e) workshops.
+
+Resposta:
+c) JAD
+"""
---
+++ Aula 01 - Atividade Verificadora de Aprendizagem - Questão 2.py
@@ -0,0 +1,18 @@
+"""Atividade verificadora de aprendizagem (continuação)
+
+Questão 2) FJPF 2006 CONAB FJPF Analista de Sistemas:
+O modelo de processo de desenvolvimento de software incremental que enfatiza um ciclo de
+desenvolvimento extremamente curto, que compreende as fases de modelagem do negócio,
+modelagem dos dados, modelagem do processo, geração da aplicação, além de teste e entrega,
+e que o desenvolvimento é conseguido pelo uso de construção baseada em componentes, é
+conhecido como modelo:
+a) sequencial linear.
+b) RAD (Rapid Application Development).
+c) de prototipagem.
+d) espiral.
+e) de desenvolvimento concorrente.
+
+Resposta:
+b) RAD
+
+"""
